a45c238c3441761ae8a9c191766093b6af63ee38
arvcode <arvindhgalaxys2@gmail.com>
2017-11-15 22:26:38 +0800

    Added sample local.mk

---
+++ buildroot-2017.02.5/local.mk
@@ -0,0 +1,7 @@
+<pkg1>_OVERRIDE_SRCDIR = /path/to/pkg1/sources
+<pkg2>_OVERRIDE_SRCDIR = /path/to/pkg2/sources
+
+#LINUX_OVERRIDE_SRCDIR = /home/bob/linux/
+#BUSYBOX_OVERRIDE_SRCDIR = /home/bob/busybox/
+
+
